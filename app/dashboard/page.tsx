9443adc41fc5d8e24fc7746d5db71d21f11aa81a
Harish Garg <rishusharma123321@gmail.com>
2024-12-22 18:32:30 +0530

    colorchanged

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -27,7 +27,7 @@ export default function DashboardPage() {
   return (
     <div>
       <Navbar />
-      <div className="flex-1 space-y-4 p-8 pt-6 bg-black text-white">
+      <div className="flex-1 space-y-4 p-8 pt-6 bg-cyan-50 text-black">
       <div className="flex items-center justify-between space-y-2">
         <h2 className="text-3xl font-bold tracking-tight">Dashboard</h2>
       </div>
